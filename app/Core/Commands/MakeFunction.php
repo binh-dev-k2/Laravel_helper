9130d40d00fc8c3e1697c8518a23546bfe32bde6
Tran Binh <binhcoder02@gmail.com>
2024-04-07 11:06:33 +0700

    update subFolder

--- app/Core/Commands/MakeFunction.php
+++ app/Core/Commands/MakeFunction.php
@@ -7,7 +7,7 @@ use Illuminate\Console\Command;
 
 class MakeFunction extends Command
 {
-    protected $signature = 'make:base {name} {--a} {--m} {--s} {--r} {--api}';
+    protected $signature = 'make:base {name} {--a} {--m} {--s} {--r} {--api} {--sub=}';
     protected $description = 'Create a new base with predefined methods in Core/Commands folder';
     protected $name;
 
@@ -20,24 +20,25 @@ class MakeFunction extends Command
     {
         $name = ucfirst($this->argument('name'));
         $options = $this->options();
+        $subFolder = $options['sub'] ?? '';
         $functionUtil->declareName($name);
 
         if ($options['m']) {
-            $functionUtil->createModel();
+            $functionUtil->createModel($subFolder);
         }
 
         if ($options['s']) {
-            $functionUtil->createModel();
+            $functionUtil->createModel($subFolder);
         }
 
         if ($options['r']) {
             $isApi = $options['api'] ? true : false;
-            $functionUtil->createRequest($isApi);
+            $functionUtil->createRequest($isApi, $subFolder);
         }
 
         if ($options['a']) {
             $isApi = $options['api'] ? true : false;
-            $functionUtil->createController($isApi);
+            $functionUtil->createController($isApi, $subFolder);
         }
 
         $this->info('Base created successfully.');
